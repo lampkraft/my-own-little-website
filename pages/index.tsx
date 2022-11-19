4090e4eca062d0c88409c49b842f992546000ff7
lampkraft <lampkraft@gmail.com>
2022-11-19 19:58:15 +0100

    prototype image section test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.scss";
+import backdrop1 from "./backdrop1.png";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>My own little website</title>
         <meta name="description" content="Personal website for testing" />
@@ -12,22 +14,34 @@ export default function Home() {
 
       <main className={styles.main}>
         <section className={styles.section}>
-          <h1 className={styles.title}>Tiny little website</h1>
+          <div className={styles.container}>
+            <h1 className={styles.title}>Tiny little website</h1>
 
-          <p className={styles.description}>
-            Hello 👋 and welcome to this <em>tiny little website</em>. Below are
-            some <strong>cool links</strong> that point to more content.
-          </p>
+            <p className={styles.description}>
+              Hello 👋 and welcome to this <em>tiny little website</em>. Below
+              are some <strong>cool links</strong> that point to more content.
+            </p>
+          </div>
         </section>
-        <section className={styles.section}></section>
         <section className={styles.section}>
-          <h2 className={styles.title}>Links</h2>
+          <div className={styles.backdrop}>
+            <Image
+              src={backdrop1}
+              alt="mysterious forest"
+              layout="intrinsic "
+            />
+          </div>
+        </section>
+        <section className={styles.section}>
+          <div className={styles.container}>
+            <h2 className={styles.title}>Links</h2>
 
-          <ul>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
+            <ul>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
         </section>
       </main>
 
